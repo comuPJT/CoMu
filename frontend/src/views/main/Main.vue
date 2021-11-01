540f6f874aabc3e3463b7847ade2b159f58f045e
김우현 <pinkfloyd951@naver.com>
2021-11-01 23:25:43 +0900

    feat : [FE] 메인페이지 구현
    서비스 설명 스크롤을 제외한 초기페이지 구현

--- frontend/src/views/main/Main.vue
+++ frontend/src/views/main/Main.vue
@@ -1,14 +1,26 @@
 <template>
-  <div>
-    메인페이지
+  <div class="mainpage">
+    <img class="logo" src="@/assets/images/logo_4X1.png"/>
+    <div class="getstart">
+      <div class="box1">
+        <img class="lp" src="@/assets/images/mainpage_lp.gif">
+        <img class="lp_text" src="@/assets/images/mainpage_lp_text.png">
+      </div>
+      <div class="box2">
+        <img class="enter_button" src="@/assets/images/mainpage_enter.png">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
+
 }
+
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "./Main.scss";
 </style>
