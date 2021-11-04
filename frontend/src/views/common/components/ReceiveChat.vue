66206ada5a072c2ff7c124ef86430483d5f41f3c
김우현 <pinkfloyd951@naver.com>
2021-11-04 20:54:53 +0900

    feat : [FE] 네비게이션 바 구현
    네비게이션바, 채팅UI, 온라인 유저 UI 구현
    
    #S05P31A304-64

---
+++ frontend/src/views/common/components/ReceiveChat.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="chat-wrapper">
+    <div class="profile-wrapper">
+      <div class="profile-wrapper-box">
+        <div class="profile-nickname">{{ nickname }}
+        </div>
+        <img
+          src="https://file.mk.co.kr/meet/neds/2015/09/image_readtop_2015_891935_14423221542127136.jpg"
+        />
+      </div>
+    </div>
+    <div class="message-wrapper message-receive">
+      {{ message }}
+      <div class="time-wrapper-receive">{{time}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ReceiveChat",
+
+  components: {},
+
+  props: ["message", "nickname", "time"],
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./SendChat.scss";
+//CSS 전부 공통이라 파일 분리 안하고 SendChat.scss사용
+</style>
---
+++ frontend/src/views/unityview/UnityView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <nav-bar></nav-bar>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "UnityView",
+
+  components: {
+  },
+
+  props: {},
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
