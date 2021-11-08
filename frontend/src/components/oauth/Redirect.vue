71fbcb30f5daa1d932c0f507b1a8858416d018e9
김우현 <pinkfloyd951@naver.com>
2021-11-08 16:38:42 +0900

    feat : [FE] 구글 로그인 연동
    구글 소셜로그인 후 토큰 로컬스토리지에 저장하도록 구현 완료
    
    #S05P31A304-99

--- frontend/src/components/oauth/Redirect.vue
+++ frontend/src/components/oauth/Redirect.vue
@@ -1,18 +1,17 @@
-<template lang="pug">
+<template>
 <div></div>
 </template>
 
 <script>
 import { mapMutations, mapActions } from 'vuex'
 
+const storage = localStorage;
+
 export default {
   created () {
     const token = this.$route.query.token
     console.log('token', token)
-    if (token) {
-      this.setToken(token)
-      this.fetchUser()
-    }
+    storage.setItem("jwt-access-token", token.substring(7));
     this.$router.replace('/')
   },
   methods: {
--- frontend/src/util/utils.js
+++ frontend/src/util/utils.js
@@ -5,8 +5,7 @@
 
 export default {
   getSocialLoginUrl (socialType) {
-      console.log(`localhost:8080/oauth2/authorization/${socialType}?redirect_uri=localhost:8081`);
-    //return `${BACKEND_URL}/oauth2/authorization/${socialType}?redirect_uri=${REDIRECT_URI}`
-    return `https://k5a304.p.ssafy.io/oauth2/authorization/${socialType}?redirect_uri=localhost:8081`;
+    //return `http://${BACKEND_URL}/oauth2/authorization/${socialType}?redirect_uri=${REDIRECT_URI}`
+    return `localhost:8080/oauth2/authorization/${socialType}?redirect_uri=localhost:8081`;
   },
 }
