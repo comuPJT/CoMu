8d37ab848fcfae7e0ee7873eb6c60e517170c1c6
jihyunhillpark <girl9505@naver.com>
2021-11-08 16:31:44 +0900

    style: [BE] 코드포맷팅
    음악공유 Service 인터페이스 코드 포맷팅

--- backend/src/main/java/com/listener/comu/domain/music/api/ShareMusicService.java
+++ backend/src/main/java/com/listener/comu/domain/music/api/ShareMusicService.java
@@ -14,7 +14,6 @@ public interface ShareMusicService {
     SharePlaylistMusicRes getPlayedMusicFromPlayList(Long roomId, String playId);
     void  deletePlayedMusicFromPlayList(Long roomId, String playId);
     void deleteMusicRequestFromPlayList(Long roomId, String playId);
-    /* TO DO - 앞뒤로 15곡씩 보여주기 - 리스트 반환*/
     List<SharePlaylistMusicRes> getPlaylistUpAndDown (Long roomId);
     List<SharePlaylistMusicRes> getHonoredMusicAndContent(Long roomId);
     /* TO DO - 명예의 전당 사연 상세보기 */
@@ -22,6 +21,7 @@ public interface ShareMusicService {
     /* TD DO - 명예의 전당 사연 삭제*/
     void deleteMusicFromHonorList(Long roomId, String playId);
     void toggleLikeMusicRequest(Long playId, Long userId);
+
     /* EVENT DRIVEN */
     /* TO DO - 재생하기 위해 다음곡 가져오기 */
     /* TO DO - 재생 후 list 조정 */
