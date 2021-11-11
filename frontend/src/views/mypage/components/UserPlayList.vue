d1ce24c646658ca87d23e6901c5ca973b13c8b25
김우현 <pinkfloyd951@naver.com>
2021-11-11 13:08:06 +0900

    feat : [FE] 내 플레이리스트 수정 drag 적용
    drag 모듈 적용
    
    #S05P31A304-97

--- frontend/src/views/mypage/components/UserPlayList.vue
+++ frontend/src/views/mypage/components/UserPlayList.vue
@@ -7,7 +7,7 @@
       <img class="line-3" src="@/assets/images/table-line.png" />
 
       <!--각각의 플레이 리스트-->
-      <div class="play-list-detail">
+      <div class="play-list-detail" @click="showModal = true">
         <div class="play-list-detail-title">플레이리스트1</div>
         <img class="img-1" src="@/assets/images/lp_now_playing.png" />
         <img class="img-2" src="@/assets/images/tempcover1.jpg" />
@@ -15,21 +15,29 @@
         <img class="img-4" src="@/assets/images/tempcover3.jpg" />
         <img class="img-5" src="@/assets/images/tempcover4.jpg" />
       </div>
-      
+
       <!--각각의 플레이 리스트 끝-->
+
+      <!-- 플레이리스트 상세보기 모달 -->
+      <user-play-list-detail v-if="showModal" @close="showModal = false">
+      </user-play-list-detail>
+      <!-- 플레이리스트 상세보기 모달 끝-->
     </div>
   </div>
 </template>
 
 <script>
+import UserPlayListDetail from "./UserPlayListDetail.vue";
 export default {
   name: "UserPlayList",
 
-  components: {},
+  components: {UserPlayListDetail},
 
   props: {},
   data() {
-    return {};
+    return {
+      showModal: false,
+    };
   },
   methods: {},
 };
