ff4883044fa1eced9e27dea0aa700a9381131dce
원지연 <yh3347@naver.com>
2021-11-11 15:06:16 +0900

    feat: [FE] API URI 설정
    - dev.js, index.js, key.js, prod.js 내용 작성
    - http-common.js 파일 API URI 부분 수정
    
    #S05P31A304-36

---
+++ frontend/src/constant/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  apiServerUri: "https://k5a304.p.ssafy.io/api",
+  apiClientUri: "https://k5a304.p.ssafy.io",
+};
--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -1,7 +1,8 @@
+import {API_SERVER_URL} from "@/constant/index.js";
 import axios from "axios";
 // import jwt_decode from 'jwt-decode';
 const storage = localStorage;
-axios.defaults.baseURL = "https://k5a304.p.ssafy.io/api/";
+axios.defaults.baseURL = API_SERVER_URL;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.put["Content-Type"] = "application/json";
 
@@ -13,12 +14,14 @@ instance.interceptors.request.use(
     // 요청을 보내기 전에 수행할 일
     // ...
     // 1. 로그인, 공용플레이리스트, 사연함의 요청은 헤더 없이 전송
-    if (true) { //조건 true 추후 스웨거보고 수정 예정 ex)config.url.split("/")[3] === "user" && config.url.split("/")[4] !== "info"
+    if (true) {
+      //조건 true 추후 스웨거보고 수정 예정 ex)config.url.split("/")[3] === "user" && config.url.split("/")[4] !== "info"
       return config;
     }
     // 2. 그 외의 요청에는 항상 토큰을 헤더에 넣어서 보냄.
     else {
-      config.headers["Authorization"] = "Bearer " + storage.getItem("jwt-access-token");
+      config.headers["Authorization"] =
+        "Bearer " + storage.getItem("jwt-access-token");
       return config;
     }
   },
@@ -35,7 +38,10 @@ instance.interceptors.response.use(
     // 응답 데이터를 가공
     // ...
     if (response.headers["authorization"]) {
-      storage.setItem("jwt-access-token", response.headers["authorization"].substring(7));
+      storage.setItem(
+        "jwt-access-token",
+        response.headers["authorization"].substring(7)
+      );
     }
     return response;
   },
