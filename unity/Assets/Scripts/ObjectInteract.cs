740c0ccb1ac843642ab4775b71f9828c01e065e7
원지연 <yh3347@naver.com>
2021-11-15 19:15:38 +0900

    feat: [Unity] 오브젝트에 가까이 갔을 때 화면 변화
    - 외곽선 표시를 위한 Shader 에셋 추가
    - 오브젝트에 외곽선 및 안내메시지 표시를 위한 맵과 오브젝트 수정
    - 오브젝트에 가까이 갔을 때 화면을 바꾸기 위한 스크립트 작성
    
    #S05P31A304-138

---
+++ unity/Assets/Scripts/ObjectInteract.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class ObjectInteract : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject message;
+
+    private Shader normal, outline1, outline2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        normal = Shader.Find("Sprites/Default");
+        outline1 = Shader.Find("Sprites/Outline1");
+        outline2 = Shader.Find("Sprites/Outline2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 플레이어가 오브젝트와 가까워지면 테두리 표시하기
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (target.CompareTag("Record"))
+            {
+                target.GetComponent<SpriteRenderer>().material.shader = outline1;
+            }
+            else
+            {
+                target.GetComponent<SpriteRenderer>().material.shader = outline2;
+            }
+
+            // 상호작용 방법 안내 메시지 표시하기
+            message.SetActive(true);
+            float tmp = gameObject.transform.position.y - target.GetComponent<RectTransform>().rect.height - 0.1f;
+            message.transform.position = new Vector3(gameObject.transform.position.x, tmp, 0);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // 플레이어가 오브젝트와 멀어지면 테두리 없애기
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            target.GetComponent<SpriteRenderer>().material.shader = normal;
+
+            // 상호작용 방법 안내 메시지 숨기기
+            message.SetActive(false);
+        }
+    }
+}
