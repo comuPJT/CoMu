dd599ab08c2ab14ce253862c2b9e536f8ea4e8b0
원지연 <yh3347@naver.com>
2021-11-16 03:40:18 +0900

    feat: [Unity] 오브젝트와 상호작용 기능 추가
    - 각 모달창 여는 함수 코드 수정 및 이름 변경
    - 모달창 열렸을 때 유니티 키보드 입력을 막기 위한 기능 추가
    - 모달창 닫았을 때 키보드 입력 활성화 시키는 기능 추가
    
    #S05P31A304-162

--- unity/Assets/Plugins/WebGLPlugin.jslib
+++ unity/Assets/Plugins/WebGLPlugin.jslib
@@ -4,44 +4,33 @@ mergeInto(LibraryManager.library, {
         return localStorage.getItem("characterNum");
     },
 
-    OpenPlaylistModal: function () {
-        localStorage.setItem("showPlayList", "TRUE");
-        localStorage.setItem("showApplyMusic", "FALSE");
-        localStorage.setItem("showTodayStory", "FALSE");
-        localStorage.setItem("showBestStory", "FALSE");
-        localStorage.setItem("showCharacter", "FALSE");
+    GetInputActive: function () {
+        var value = localStorage.getItem("isUnityInputActive");
+        return value !== null;
     },
 
-    OpenApplyMusicModal: function () {
-        localStorage.setItem("showPlayList", "FALSE");
-        localStorage.setItem("showApplyMusic", "TRUE");
-        localStorage.setItem("showTodayStory", "FALSE");
-        localStorage.setItem("showBestStory", "FALSE");
-        localStorage.setItem("showCharacter", "FALSE");
+    SetInputInactive: function () {
+        localStorage.removeItem("isUnityInputActive");
+    },
+
+    OpenPlaylistModal: function () {
+        localStorage.setItem("showPlayList", "TRUE");
+    },
+
+    OpenPlayListAddModal: function () {
+        localStorage.setItem("showPlayListAdd", "TRUE");
     },
 
     OpenTodayStoryModal: function () {
-        localStorage.setItem("showPlayList", "FALSE");
-        localStorage.setItem("showApplyMusic", "FALSE");
         localStorage.setItem("showTodayStory", "TRUE");
-        localStorage.setItem("showBestStory", "FALSE");
-        localStorage.setItem("showCharacter", "FALSE");
     },
 
     OpenBestStoryModal: function () {
-        localStorage.setItem("showPlayList", "FALSE");
-        localStorage.setItem("showApplyMusic", "FALSE");
-        localStorage.setItem("showTodayStory", "FALSE");
         localStorage.setItem("showBestStory", "TRUE");
-        localStorage.setItem("showCharacter", "FALSE");
     },
 
     OpenCharacterModal: function () {
-        localStorage.setItem("showPlayList", "FALSE");
-        localStorage.setItem("showApplyMusic", "FALSE");
-        localStorage.setItem("showTodayStory", "FALSE");
-        localStorage.setItem("showBestStory", "FALSE");
         localStorage.setItem("showCharacter", "TRUE");
-    }
+    },
 
 });
--- unity/Assets/Scripts/ObjectInteract.cs
+++ unity/Assets/Scripts/ObjectInteract.cs
@@ -3,13 +3,21 @@ using System.Runtime.InteropServices;
 
 public class ObjectInteract : MonoBehaviour
 {
+    // 키보드 입력 설정 확인
+    [DllImport("__Internal")]
+    private static extern bool GetInputActive();
+
+    // 키보드 입력 설정 변경
+    [DllImport("__Internal")]
+    private static extern void SetInputInactive();
+
     // 플레이리스트 모달창 열기
     [DllImport("__Internal")]
     private static extern void OpenPlaylistModal();
 
     // 노래 신청 모달창 열기
     [DllImport("__Internal")]
-    private static extern void OpenApplyMusicModal();
+    private static extern void OpenPlayListAddModal();
 
     // 오늘의 사연 모달창 열기
     [DllImport("__Internal")]
@@ -30,7 +38,6 @@ public class ObjectInteract : MonoBehaviour
 
     private Shader normal, outline1, outline2;
 
-    // Start is called before the first frame update
     void Start()
     {
         isTrigger = false;
@@ -39,19 +46,18 @@ public class ObjectInteract : MonoBehaviour
         outline2 = Shader.Find("Sprites/Outline2");
     }
 
-    // Update is called once per frame
     void Update()
     {
         // 상호작용 가능 범위 내에서 X키를 누르면 기능 실행
         if (isTrigger && Input.GetKeyUp(KeyCode.X))
         {
-            if (gameObject.CompareTag("Playlist"))
+            if (gameObject.CompareTag("PlayList"))
             {
                 OpenPlaylistModal();
             }
-            else if (gameObject.CompareTag("ApplyMusic"))
+            else if (gameObject.CompareTag("PlayListAdd"))
             {
-                OpenApplyMusicModal();
+                OpenPlayListAddModal();
             }
             else if (gameObject.CompareTag("TodayStory"))
             {
@@ -65,6 +71,16 @@ public class ObjectInteract : MonoBehaviour
             {
                 OpenCharacterModal();
             }
+
+            // 모달창이 열리면 유니티에서의 키보드 입력 막기
+            SetInputInactive();
+            UnityEngine.WebGLInput.captureAllKeyboardInput = false;
+        }
+
+        // 유니티 키보드 입력 활성화 시키기
+        if (GetInputActive())
+        {
+            UnityEngine.WebGLInput.captureAllKeyboardInput = true;
         }
     }
 
@@ -73,6 +89,7 @@ public class ObjectInteract : MonoBehaviour
         // 플레이어가 오브젝트와 가까워지면 테두리 표시하기
         if (collision.gameObject.CompareTag("Player"))
         {
+            // 에셋에 따라 테두리 굵기 다르게 설정
             if (target.CompareTag("Record"))
             {
                 target.GetComponent<SpriteRenderer>().material.shader = outline1;
