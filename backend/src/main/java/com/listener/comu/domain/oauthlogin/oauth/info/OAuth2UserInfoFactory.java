7e5e278dbcccc57c12a14eba69e0689772d0f3e0
sachieve7 <sachieve7@naver.com>
2021-11-15 21:38:01 +0900

    feat: [BE] 네이버 로그인 구현 #S05P31A304-106

--- backend/src/main/java/com/listener/comu/domain/oauthlogin/oauth/info/OAuth2UserInfoFactory.java
+++ backend/src/main/java/com/listener/comu/domain/oauthlogin/oauth/info/OAuth2UserInfoFactory.java
@@ -3,8 +3,8 @@ package com.listener.comu.domain.oauthlogin.oauth.info;
 import com.listener.comu.domain.oauthlogin.oauth.entity.ProviderType;
 //import com.listener.comu.domain.oauthlogin.oauth.info.impl.FacebookOAuth2UserInfo;
 import com.listener.comu.domain.oauthlogin.oauth.info.impl.GoogleOAuth2UserInfo;
+import com.listener.comu.domain.oauthlogin.oauth.info.impl.NaverOAuth2UserInfo;
 //import com.listener.comu.domain.oauthlogin.oauth.info.impl.KakaoOAuth2UserInfo;
-//import com.listener.comu.domain.oauthlogin.oauth.info.impl.NaverOAuth2UserInfo;
 
 import java.util.Map;
 
@@ -13,7 +13,7 @@ public class OAuth2UserInfoFactory {
         switch (providerType) {
             case GOOGLE: return new GoogleOAuth2UserInfo(attributes);
 //            case FACEBOOK: return new FacebookOAuth2UserInfo(attributes);
-//            case NAVER: return new NaverOAuth2UserInfo(attributes);
+            case NAVER: return new NaverOAuth2UserInfo(attributes);
 //            case KAKAO: return new KakaoOAuth2UserInfo(attributes);
             default: throw new IllegalArgumentException("Invalid Provider Type.");
         }
