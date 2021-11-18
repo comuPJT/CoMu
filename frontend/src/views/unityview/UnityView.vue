0f72a7acc17bfbeec480753ce3b2151189a8f32d
김우현 <pinkfloyd951@naver.com>
2021-11-18 11:00:29 +0900

    fix : [FE] 유니티 뷰 테스트코드 삭제

--- frontend/src/views/unityview/UnityView.vue
+++ frontend/src/views/unityview/UnityView.vue
@@ -5,7 +5,6 @@
     ></nav-bar>
     <!-- 유니티 화면 -->
     <div id="unity">
-      <button @click="showModalPlayList=true">dsds</button>
       <div
         v-if="isShowBlind"
         class="blind"
@@ -16,7 +15,6 @@
         </div>
       </div>
       <unity
-        v-if="false"
         src="unity/Build/unity.json"
         unityLoader="unity/Build/UnityLoader.js"
         ref="comu"
