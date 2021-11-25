69997a6d3ae035db250ea1aaf43055d8e9459474
jihyunhillpark <girl9505@naver.com>
2021-11-25 23:12:58 +0900

    fix: [BE] 좀비 프로세스 문제 해결 by Apache Commons Exec

--- backend/src/main/java/com/listener/comu/domain/music/api/StreamingService.java
+++ backend/src/main/java/com/listener/comu/domain/music/api/StreamingService.java
@@ -6,6 +6,10 @@ import com.listener.comu.domain.music.domain.SharePlaylistMusic;
 import com.listener.comu.domain.music.domain.Status;
 import com.listener.comu.util.S3Uploader;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecuteResultHandler;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteWatchdog;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.scheduling.annotation.Async;
@@ -13,7 +17,6 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -27,16 +30,21 @@ public class StreamingService {
         nowPlay.setStatus(Status.READY);
         operations.put(nowMusicKey, "room:"+roomId , nowPlay);
 
-        Runtime rt = Runtime.getRuntime();
-        String cmd = "sh /home/stream.sh " + roomId + " " + musicName; // stream.sh가 위치한 절대경로를 쓸 것
+        String cmd = "sh stream.sh " + roomId + " " + musicName; // stream.sh가 위치한 절대경로를 쓸 것
         //    String cmd = "%userprofile%/stream.bat " + roomId + " " + musicName;
         try {
             System.out.println("Streaming start...");
-            Process pr = rt.exec(cmd);
-            if(!pr.waitFor(6, TimeUnit.MINUTES)) {
-                //timeout - kill the process.
-                pr.destroyForcibly();
-            }
+            DefaultExecutor executor = new DefaultExecutor();
+            ExecuteWatchdog watchdog = new ExecuteWatchdog(60000*6); //6 mins Timeout
+            executor.setWatchdog(watchdog);
+            DefaultExecuteResultHandler resultHandler = new DefaultExecuteResultHandler();
+
+            CommandLine cmdLine = CommandLine.parse(cmd);
+            executor.execute(cmdLine, resultHandler);
+            resultHandler.waitFor();
+            int exitCode = resultHandler.getExitValue();
+            System.out.println("FFMPEG Streaming exited " + exitCode);
+
             operations.delete(nowMusicKey, "room:" + roomId);
             if( !nowPlay.getPlayId().equals("Anonymous")) {
                 nowPlay.setStatus(Status.DONE);
@@ -50,15 +58,15 @@ public class StreamingService {
 
     @Async
     public void executeDownloadAndUploadToS3(Music music){
-        String cmd = "youtube-dl -f 160+140 -o /home/" + music.getSpotifyId() + ".%(ext)s " + music.getSource();
-//        String cmd = "bash -c \"youtube-dl -f 160+140 -o %userportfolio%/" + music.getSpotifyId() + ".%(ext)s " + music.getSource() + "\"";
+        String cmd = "youtube-dl -f 160+140 -o " + music.getSpotifyId() + ".%(ext)s " + music.getSource();
+//        String cmd = "bash -c \"youtube-dl -f 160+140 -o " + music.getSpotifyId() + ".%(ext)s " + music.getSource() + "\"";
         Runtime rt = Runtime.getRuntime();
         try {
             Process pr = rt.exec(cmd);
             pr.waitFor();
             pr.destroy();
             String sourceFilepath = music.getSpotifyId() + ".mp4";
-//            String sourceFilepath = "bash -c \"src/main/resources/static/" + music.getSpotifyId() + ".mp4\"";
+//            String sourceFilepath = "bash -c \"" + music.getSpotifyId() + ".mp4\"";
             s3Uploader.dirUpload(new File(sourceFilepath),"static");
             music.setOnCloud(1);
             musicRepository.save(music);
